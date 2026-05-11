ef5c04e62c97b924db13076f1f8ac0fd4429824a
Max <maxwellpegg7@gmail.com>
2026-05-11 22:10:15 +0200

    Added more actions per the tutorial

--- Game/event_handler.gd
+++ Game/event_handler.gd
@@ -7,13 +7,13 @@ func get_action() -> Action:
 	var action: Action = null
 	
 	if Input.is_action_just_pressed("ui_up"):
-		action = MovementAction.new(0, -1)
+		action = BumpAction.new(0, -1)
 	elif Input.is_action_just_pressed("ui_down"):
-		action = MovementAction.new(0, 1)
+		action = BumpAction.new(0, 1)
 	elif Input.is_action_just_pressed("ui_left"):
-		action = MovementAction.new(-1, 0)
+		action = BumpAction.new(-1, 0)
 	elif Input.is_action_just_pressed("ui_right"):
-		action = MovementAction.new(1, 0)
+		action = BumpAction.new(1, 0)
 	
 	elif Input.is_action_just_pressed("ui_click"):
 		var input_position: Vector2i = game.camera.get_global_mouse_position()
@@ -22,7 +22,7 @@ func get_action() -> Action:
 		var tile: Tile = map.get_tile_xy(grid_position.x, grid_position.y)
 		if tile.is_explored: 
 			var delta: Vector2i = grid_position - game.player.grid_position
-			action = MovementAction.new(delta.x, delta.y)
+			action = BumpAction.new(delta.x, delta.y)
 		
 			
 	
